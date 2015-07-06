8641785b753a79e78765f11370d37c12287db675
cechishi <shicechi@gmail.com>
2015-07-06 07:50:13 -0700

    Initial commit.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Police shootings
 
 This repository is a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/police-shooting).
+
+The web page is hosted at http://students.washington.edu/cechishi/info343/police-shooting
--- js/map.js
+++ js/map.js
@@ -1,32 +1,47 @@
 // Function to draw your map
-var drawMap() = function() {
-
-  // Create map and set viewd
- 
-
-  // Create an tile layer variable using the appropriate url
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
-}
+var drawMap = function() {
+    // Create map and set view
+    var map = L.map('container');
+    map.setView([37.8, -96], 5);
+    // Create an tile layer variable using the appropriate url
+    var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {maxZoom: 13});
+    // Add the layer to your map
+    layer.addTo(map);
+    // Execute your function to get data
+    getData(map);
+};
 
 // Function for getting data
-var getData = function() {
+var getData = function(map) {
+    // Execute an AJAX request to get the data in data/response.js
+    $.ajax({
+        url: 'data/response.json',
+        type: 'get',
+        success: function(data) {
+            customBuild(data, map);
+        }
+    })
+};
+// Do something creative with the data here!
+var customBuild = function(data, map) {
+    console.log(data[0]);
+    console.log(data[1]);
+    console.log(data[3]);
 
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
-
-}
-
-// Do something creative with the data here!  
-var customBuild = function() {
-
-  
-}
-
+    var markers = L.markerClusterGroup({showCoverageOnHover: false,
+                                        spiderfyDistanceMultiplier: 4,
+                                        maxClusterRadius:60});
+    data.map(function(d) {
+        var popup = d['Victim Name'] + ' was shot by an officer from ' + d['Agency Name'] + ' ' + d['Armed or Unarmed?'] + '.<br><a href="'+d['Source Link']+'" target="_blank">Read more</a>';
+        var marker;
+        if (d['Hit or Killed?'] == 'Killed') {
+            marker = L.circleMarker(new L.LatLng(d.lat, d.lng), {color: 'red', opacity: 1});
+        } else {
+            marker = L.circleMarker(new L.LatLng(d.lat, d.lng), {color: 'gold', opacity: 1});
+        }
+        marker.bindPopup(popup);
+        markers.addLayer(marker);
+    });
+    map.addLayer(markers);
+};
 
