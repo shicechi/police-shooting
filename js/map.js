c2bf2990d637ecd0533d19f2e888147a7817d52b
cechishi <shicechi@gmail.com>
2015-07-06 16:01:52 -0700

    Markers showing by specifying the data type, attribution removed, comments added, popup on mouse over

--- js/map.js
+++ js/map.js
@@ -1,10 +1,10 @@
-// Function to draw your map
+// Function to draw the map
 var drawMap = function() {
     // Create map and set view
-    var map = L.map('container');
-    map.setView([37.8, -96], 5);
+    var map = L.map('container', {attributionControl: false}); // removes default leaflet attribution
+    map.setView([37.8, -96], 5); // use continental America as default view
     // Create an tile layer variable using the appropriate url
-    var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {maxZoom: 13});
+    var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {maxZoom: 13}); // applies a max zoom level for clarity
     // Add the layer to your map
     layer.addTo(map);
     // Execute your function to get data
@@ -19,26 +19,32 @@ var getData = function(map) {
         type: 'get',
         dataType: 'json',
         success: function(data) {
-            console.log(data[0]);
             customBuild(data, map);
         }
     })
 };
-// Do something creative with the data here!
+// Custom features for the map
 var customBuild = function(data, _map) {
-    console.log(data[1]);
     var markers = L.markerClusterGroup({showCoverageOnHover: false,
                                         spiderfyDistanceMultiplier: 4,
                                         maxClusterRadius:60});
     data.map(function(d) {
+        // Content for popup
         var popup = d['Victim Name'] + ' was shot by an officer from ' + d['Agency Name'] + ' ' + d['Armed or Unarmed?'] + '.<br><a href="'+d['Source Link']+'" target="_blank">Read more</a>';
         var marker;
+        // Red marker if killed, yellow if hit
         if (d['Hit or Killed?'] == 'Killed') {
             marker = L.circleMarker(new L.LatLng(d.lat, d.lng), {color: 'red', opacity: 1});
         } else {
             marker = L.circleMarker(new L.LatLng(d.lat, d.lng), {color: 'gold', opacity: 1});
         }
-        marker.bindPopup(popup);
+        marker.bindPopup(popup, {closeButton: false, offset: L.point(0, -20)});
+        marker.on('mouseover', function (e) {
+            this.openPopup();
+        });
+        marker.on('mouseout', function (e) {
+            this.closePopup();
+        });
         markers.addLayer(marker);
     });
     _map.addLayer(markers);
